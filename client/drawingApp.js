cea9469a1ee27a84e972310f28dafdafaa141906
Evangelos Papakonstantis <vangos@lazygalaxy.com>
2016-02-08 22:54:44 +0100

    format files

--- client/drawingApp.js
+++ client/drawingApp.js
@@ -2,97 +2,97 @@ points = new Meteor.Collection('pointsCollection');
 var canvas;
 
 // we use these for drawing more interesting shapes
-var lastX=0;
-var lastY=0;
+var lastX = 0;
+var lastY = 0;
 var strokeWidth = 1;
-var thickness=1;
+var thickness = 1;
 var strokeColor = "black";
 
-Meteor.startup( function() {
-  canvas = new Canvas();
+Meteor.startup(function () {
+    canvas = new Canvas();
 
-  Deps.autorun( function() {
-    var data = points.find({}).fetch();
+    Deps.autorun(function () {
+        var data = points.find({}).fetch();
 
-    if (canvas) {
-      canvas.draw(data);
-    }
-  });
+        if (canvas) {
+            canvas.draw(data);
+        }
+    });
 });
 
 Template.wall.events({
 
-  "click button.clear": function (event) {
-    Meteor.call('clear', function() {
-      canvas.clear();
-    });
-  },
+    "click button.clear": function (event) {
+        Meteor.call('clear', function () {
+            canvas.clear();
+        });
+    },
 
-  //choose a color. Initialise the last vals, otherwise a stray line will appear.
+    //choose a color. Initialise the last vals, otherwise a stray line will appear.
 
-  "click button.red": function () {
-    lastX=0;
-    lastY=0;
-    strokeColor = "red";
-  },
+    "click button.red": function () {
+        lastX = 0;
+        lastY = 0;
+        strokeColor = "red";
+    },
 
-  "click button.black": function () {
-    lastX=0;
-    lastY=0;
-    strokeColor = "black";
-  },
+    "click button.black": function () {
+        lastX = 0;
+        lastY = 0;
+        strokeColor = "black";
+    },
 
-  "click button.white": function () {
-    lastX=0;
-    lastY=0;
-    strokeColor = "white";
-  },
+    "click button.white": function () {
+        lastX = 0;
+        lastY = 0;
+        strokeColor = "white";
+    },
 
-  "click button.blue": function () {
-    lastX=0;
-    lastY=0;
-    strokeColor = "blue";
-  },
+    "click button.blue": function () {
+        lastX = 0;
+        lastY = 0;
+        strokeColor = "blue";
+    },
 
-  "click button.green": function () {
-    lastX=0;
-    lastY=0;
-    strokeColor = "green";
-  },
+    "click button.green": function () {
+        lastX = 0;
+        lastY = 0;
+        strokeColor = "green";
+    },
 
-  "click button.thicker": function () {
+    "click button.thicker": function () {
 
-    thickness+=1;
+        thickness += 1;
 
-  },
+    },
 
-  "click button.thinner": function () {
+    "click button.thinner": function () {
 
-    if (thickness > 0) {
-      thickness-=1;
-    }
-  },
+        if (thickness > 0) {
+            thickness -= 1;
+        }
+    },
 
 
 
 })
 
-var markPoint = function() {
+var markPoint = function () {
 
-  var offset = $('#canvas').offset();
+    var offset = $('#canvas').offset();
 
-// In the first frame, lastX and lastY are 0.
-// This means the line gets drawn to the top left of the screen
-// Which is annoying, so we test for this and stop it happening.
+    // In the first frame, lastX and lastY are 0.
+    // This means the line gets drawn to the top left of the screen
+    // Which is annoying, so we test for this and stop it happening.
 
-      if (lastX==0) {// check that x was something not top-left. should probably set this to -1
+    if (lastX == 0) { // check that x was something not top-left. should probably set this to -1
         lastX = (event.pageX - offset.left);
         lastY = (event.pageY - offset.top);
-      }
-      points.insert({
+    }
+    points.insert({
         //this draws a point exactly where you click the mouse
-      // x: (event.pageX - offset.left),
-      // y: (event.pageY - offset.top)});
+        // x: (event.pageX - offset.left),
+        // y: (event.pageY - offset.top)});
 
 
         //We can do more interesting stuff
@@ -101,8 +101,8 @@ var markPoint = function() {
 
 
         //1) Algorithmic mouse follower
-      // x: (event.pageX - offset.left)+(Math.cos((event.pageX/10  ))*30),
-      // y: (event.pageY - offset.top)+(Math.sin((event.pageY)/10)*30)});
+        // x: (event.pageX - offset.left)+(Math.cos((event.pageX/10  ))*30),
+        // y: (event.pageY - offset.top)+(Math.sin((event.pageY)/10)*30)});
 
         //2) draw a line - requires you to change the code in drawing.js
         x: (event.pageX - offset.left),
@@ -119,28 +119,28 @@ var markPoint = function() {
         c: strokeColor,
 
 
-      }); // end of points.insert()
+    }); // end of points.insert()
 
-        lastX = (event.pageX - offset.left);
-        lastY = (event.pageY - offset.top);
+    lastX = (event.pageX - offset.left);
+    lastY = (event.pageY - offset.top);
 
 }
 
 Template.canvas.events({
-  'click': function (event) {
-    markPoint();
-  },
-  'mousedown': function (event) {
-    Session.set('draw', true);
-  },
-  'mouseup': function (event) {
-    Session.set('draw', false);
-    lastX=0;
-    lasyY=0;
-  },
-  'mousemove': function (event) {
-    if (Session.get('draw')) {
-      markPoint();
+    'click': function (event) {
+        markPoint();
+    },
+    'mousedown': function (event) {
+        Session.set('draw', true);
+    },
+    'mouseup': function (event) {
+        Session.set('draw', false);
+        lastX = 0;
+        lasyY = 0;
+    },
+    'mousemove': function (event) {
+        if (Session.get('draw')) {
+            markPoint();
+        }
     }
-  }
 });
